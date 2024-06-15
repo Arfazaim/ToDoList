cc2d34cf4aa6ac061fc431edde1ab5d2ba3007c3
Arfa Zaim Al Murtadlo <arfazaim0202@gmail.com>
2024-06-15 20:30:30 +0700

    r

--- login/loginrodolist.php
+++ login/loginrodolist.php
@@ -22,21 +22,21 @@
 <!--===============================================================================================-->
 </head>
 <body>
-	
+<form action="proseslogin.php" method="post" class="signin-form">	
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100">
 				<div class="login100-pic js-tilt" data-tilt>
 					<img src="images/img-01.png" alt="IMG">
 				</div>
-
+				
 				<form class="login100-form validate-form">
 					<span class="login100-form-title">
-						Member Login
+						Login
 					</span>
 
 					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-						<input class="input100" type="text" name="email" placeholder="Email">
+						<input class="input100" type="text" name="username" placeholder="username">
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
 							<i class="fa fa-envelope" aria-hidden="true"></i>
@@ -76,6 +76,7 @@
 			</div>
 		</div>
 	</div>
+</form>
 	
 	
 
